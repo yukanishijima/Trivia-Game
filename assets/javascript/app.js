db20df5e4bd3adbf11b075e6008474a7a8a87c1d
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-06-23 17:38:32 -0400

    Fix vh issue with mobile

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -237,4 +237,16 @@ $(document).ready(function () {
   });
 });
 
+//for address bar for mobile screen
+//get the viewport height and multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+//set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+//listen to the resize event
+window.addEventListener('resize', () => {
+  //execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
